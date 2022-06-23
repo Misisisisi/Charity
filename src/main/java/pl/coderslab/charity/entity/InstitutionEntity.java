3070eba52ab2bb658bd0af3634d5e99ae1e0a306
misisisisi <aniazajac8@gmail.com>
2022-06-23 20:28:14 +0200

    add InstitutionEntity

---
+++ src/main/java/pl/coderslab/charity/entity/InstitutionEntity.java
@@ -0,0 +1,19 @@
+package pl.coderslab.charity.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class InstitutionEntity {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String description;
+}
