068c2138f9bfef1f96d94dd764d1b0c97711fb2a
misisisisi <aniazajac8@gmail.com>
2022-06-28 19:18:53 +0200

    update InstitutionService

--- src/main/java/pl/coderslab/charity/service/InstitutionService.java
+++ src/main/java/pl/coderslab/charity/service/InstitutionService.java
@@ -1,7 +1,12 @@
 package pl.coderslab.charity.service;
 
 import org.springframework.stereotype.Service;
+import pl.coderslab.charity.entity.InstitutionEntity;
+
+import java.util.List;
 
 @Service
 public interface InstitutionService {
+
+    List <InstitutionEntity> loadInstitutions();
 }
