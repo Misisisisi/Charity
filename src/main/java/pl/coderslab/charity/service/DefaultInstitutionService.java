ab2b7b968876b45e51da589561ff8f87309b51ad
misisisisi <aniazajac8@gmail.com>
2022-06-28 19:17:03 +0200

    add DefaultInstitutionService

--- src/main/java/pl/coderslab/charity/service/DefaultInstitutionService.java
+++ src/main/java/pl/coderslab/charity/service/DefaultInstitutionService.java
@@ -2,8 +2,18 @@ package pl.coderslab.charity.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.coderslab.charity.entity.InstitutionEntity;
+import pl.coderslab.charity.repository.InstitutionRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class DefaultInstitutionService implements InstitutionService{
+public class DefaultInstitutionService implements InstitutionService {
+
+    private final InstitutionRepository institutionRepository;
+
+    public List<InstitutionEntity> loadInstitutions() {
+        return institutionRepository.findAll();
+    }
 }
