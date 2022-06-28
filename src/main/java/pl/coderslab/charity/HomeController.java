5e9d0f83e2fa89bc9830cc4d723f143b6281f3f9
misisisisi <aniazajac8@gmail.com>
2022-06-28 19:18:04 +0200

    update HomeController

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -1,16 +1,38 @@
 package pl.coderslab.charity;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.entity.InstitutionEntity;
+import pl.coderslab.charity.service.DefaultDonationService;
+import pl.coderslab.charity.service.DefaultInstitutionService;
+
+import java.util.List;
 
 
 @Controller
+@RequestMapping("/")
+@RequiredArgsConstructor
 public class HomeController {
 
+    private final DefaultInstitutionService institutionService;
 
-    @RequestMapping("/")
-    public String homeAction(Model model){
+    private final DefaultDonationService donationService;
+
+    @GetMapping
+    public String homeAction(Model model) {
+        List<InstitutionEntity> institutions = institutionService.loadInstitutions();
+        model.addAttribute("institutions", institutions);
+
+        Integer sumOfBags = donationService.sumOfBags();
+        model.addAttribute("sumOfBags", sumOfBags);
+
+        Integer sumOfDonations = donationService.countDonations();
+        model.addAttribute("sumOfDonations", sumOfDonations);
         return "index";
     }
+
 }
