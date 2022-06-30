a461e3ac1cdb8507e2110e14b99fba66799ff64f
misisisisi <aniazajac8@gmail.com>
2022-06-30 21:45:21 +0200

    update HomeController

--- src/main/java/pl/coderslab/charity/controller/HomeController.java
+++ src/main/java/pl/coderslab/charity/controller/HomeController.java
@@ -1,10 +1,9 @@
-package pl.coderslab.charity;
+package pl.coderslab.charity.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.charity.entity.InstitutionEntity;
 import pl.coderslab.charity.service.DefaultDonationService;
