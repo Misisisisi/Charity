6e497d1baf3c606484945542899d827ba6ac693e
misisisisi <aniazajac8@gmail.com>
2022-06-30 21:43:54 +0200

    update DonationService

--- src/main/java/pl/coderslab/charity/service/DonationService.java
+++ src/main/java/pl/coderslab/charity/service/DonationService.java
@@ -1,8 +1,14 @@
 package pl.coderslab.charity.service;
 
+import pl.coderslab.charity.entity.CategoryEntity;
+
+import java.util.List;
+
 public interface DonationService {
 
     Integer sumOfBags();
 
     Integer countDonations();
+
+
 }
