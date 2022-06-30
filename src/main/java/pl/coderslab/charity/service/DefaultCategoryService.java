015487332faabc9205233c7196074e68e4eac556
misisisisi <aniazajac8@gmail.com>
2022-06-30 21:42:30 +0200

    add loadCategories method

--- src/main/java/pl/coderslab/charity/service/DefaultCategoryService.java
+++ src/main/java/pl/coderslab/charity/service/DefaultCategoryService.java
@@ -2,8 +2,18 @@ package pl.coderslab.charity.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.coderslab.charity.entity.CategoryEntity;
+import pl.coderslab.charity.repository.CategoryRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class DefaultCategoryService implements CategoryService{
+
+    private final CategoryRepository categoryRepository;
+    @Override
+    public List<CategoryEntity> loadCategories() {
+        return categoryRepository.findAll();
+    }
 }
