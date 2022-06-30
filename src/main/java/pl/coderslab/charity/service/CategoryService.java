aeb4d727e5a50c6ffade9131fd8c587d75f8d9fd
misisisisi <aniazajac8@gmail.com>
2022-06-30 21:42:22 +0200

    add loadCategories method

--- src/main/java/pl/coderslab/charity/service/CategoryService.java
+++ src/main/java/pl/coderslab/charity/service/CategoryService.java
@@ -1,7 +1,12 @@
 package pl.coderslab.charity.service;
 
 import org.springframework.stereotype.Service;
+import pl.coderslab.charity.entity.CategoryEntity;
+
+import java.util.List;
 
 @Service
 public interface CategoryService {
+
+    List<CategoryEntity> loadCategories();
 }
