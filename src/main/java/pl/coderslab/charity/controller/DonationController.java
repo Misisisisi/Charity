3cc007652b1c505c739febc70523b9040395c5b8
misisisisi <aniazajac8@gmail.com>
2022-07-04 20:43:38 +0200

    update DonationController

--- src/main/java/pl/coderslab/charity/controller/DonationController.java
+++ src/main/java/pl/coderslab/charity/controller/DonationController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.charity.entity.CategoryEntity;
+import pl.coderslab.charity.entity.DonationEntity;
+import pl.coderslab.charity.entity.InstitutionEntity;
 import pl.coderslab.charity.service.DefaultCategoryService;
+import pl.coderslab.charity.service.DefaultInstitutionService;
 
 import java.util.List;
 
@@ -17,11 +20,16 @@ import java.util.List;
 public class DonationController {
 
     private final DefaultCategoryService categoryService;
+    private final DefaultInstitutionService institutionService;
 
     @GetMapping("donations")
     public String processView (Model model) {
+       model.addAttribute("donation", new DonationEntity()) ;
         List<CategoryEntity> categories = categoryService.loadCategories();
         model.addAttribute("categories", categories);
+
+        List<InstitutionEntity> institutions = institutionService.loadInstitutions();
+        model.addAttribute("institutions", institutions);
         return "donation";
     }
 
