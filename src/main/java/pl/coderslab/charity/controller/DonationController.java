0f182a2082b0c40e29fe4607cd0c112499df518f
misisisisi <aniazajac8@gmail.com>
2022-07-05 21:45:02 +0200

    add postMapping to save Donation

--- src/main/java/pl/coderslab/charity/controller/DonationController.java
+++ src/main/java/pl/coderslab/charity/controller/DonationController.java
@@ -3,15 +3,19 @@ package pl.coderslab.charity.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.charity.entity.CategoryEntity;
 import pl.coderslab.charity.entity.DonationEntity;
 import pl.coderslab.charity.entity.InstitutionEntity;
+import pl.coderslab.charity.repository.DonationRepository;
 import pl.coderslab.charity.service.DefaultCategoryService;
 import pl.coderslab.charity.service.DefaultInstitutionService;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -21,6 +25,7 @@ public class DonationController {
 
     private final DefaultCategoryService categoryService;
     private final DefaultInstitutionService institutionService;
+    private final DonationRepository donationRepository;
 
     @GetMapping("donations")
     public String processView (Model model) {
@@ -33,10 +38,13 @@ public class DonationController {
         return "donation";
     }
 
-    @GetMapping("donations/step2")
-    public String step2 (Model model) {
-
-        return "donation";
+    @PostMapping("/donations")
+    public String donationData(@ModelAttribute("donation") @Valid DonationEntity donation, BindingResult result) {
+        if (result.hasErrors()) {
+            return "donation";
+        }
+        donationRepository.save(donation);
+        return "donationConfirm";
     }
 
 }
