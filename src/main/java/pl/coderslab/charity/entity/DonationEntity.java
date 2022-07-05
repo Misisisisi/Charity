9699a7887b592554ebc195852f8a2e48c1720c4b
misisisisi <aniazajac8@gmail.com>
2022-07-05 21:45:29 +0200

    Validation of DonationEntity

--- src/main/java/pl/coderslab/charity/entity/DonationEntity.java
+++ src/main/java/pl/coderslab/charity/entity/DonationEntity.java
@@ -2,44 +2,52 @@ package pl.coderslab.charity.entity;
 
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.NumberFormat;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.rmi.MarshalException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
 @Entity
-@Table(name="donations")
+@Table(name = "donations")
 @Getter
 @Setter
 @ToString
 @EqualsAndHashCode(of = "id")
 @NoArgsConstructor
-@AllArgsConstructor @Builder
+@AllArgsConstructor
+@Builder
 public class DonationEntity {
 
     @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @NotNull
+    @NotNull (message = "Podaj właściwą ilość worków")
     private int quantity;
     @ManyToMany
     @JoinColumn(name = "categories_id")
-    private List <CategoryEntity> categories;
+    private List<CategoryEntity> categories;
     @ManyToOne
     @JoinColumn(name = "institution_id")
     private InstitutionEntity institution;
-    @NotNull
+    @NotNull(message = "Podaj nazwę ulicy")
     private String street;
-    @NotNull
+    @NotNull(message = "Podaj nazwę miasta")
     private String city;
-    @NotNull
+    @NumberFormat(pattern = "yyyy-MM-dd")
+    @NotNull(message = "Podaj prawidłowy kod pocztowy")
     private String zipCode;
-    @NotNull
+    @NumberFormat(pattern = "d{9}")
+    @NotNull(message = "Podaj numer telefonu")
+    private String phone;
+    @NotNull (message = "Podaj datę odbioru przesyłki")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate pickUpDate;
     @NotNull
+    @DateTimeFormat (pattern = "hh:mm")
     private LocalTime pickUpTime;
 
     private String pickUpComment;
